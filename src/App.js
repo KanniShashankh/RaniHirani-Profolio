38db032fb2b1f870805b34ac5c546e93cc765312
K Shashankh Vasudev <63902267+KanniShashankh@users.noreply.github.com>
2024-05-07 19:12:13 +0000

    delete build

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // import React from 'react';
 // import "./index.css";
-// import Home from './routes/Home';
-// import About from './routes/About';
-// import Project from './routes/Project';
-// import Contact from './routes/Contact';
+import Home from './routes/Home';
+import About from './routes/About';
+import Project from './routes/Project';
+import Contact from './routes/Contact';
 
-// import {Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 function App() {
   return (
     <>
-    <> HELLO WORLD </>
-    {/* <Routes>
+    {/* hello world */}
+    <Routes>
       <Route path ="/RaniHirani-Profolio" element ={<Home/>} />
       <Route path = "/project" element={<Project/>} />
       <Route path ="/about" element = {<About/>}/>
       <Route path="/contact" element={<Contact/>} />
-    </Routes> */}
+    </Routes>
     </>
   );
 }
