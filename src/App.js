441c9007aabbf0f6ebf1860a154a98f42adfacd1
K Shashankh Vasudev <63902267+KanniShashankh@users.noreply.github.com>
2024-05-07 23:17:25 +0530

    yo? nuked

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import "./index.css";
-import Home from './routes/Home';
-import About from './routes/About';
-import Project from './routes/Project';
-import Contact from './routes/Contact';
+// import React from 'react';
+// import "./index.css";
+// import Home from './routes/Home';
+// import About from './routes/About';
+// import Project from './routes/Project';
+// import Contact from './routes/Contact';
 
-import {Route, Routes} from "react-router-dom";
+// import {Route, Routes} from "react-router-dom";
 function App() {
   return (
     <>
-    <Routes>
+    <> HELLO WORLD </>
+    {/* <Routes>
       <Route path ="/RaniHirani-Profolio" element ={<Home/>} />
       <Route path = "/project" element={<Project/>} />
       <Route path ="/about" element = {<About/>}/>
       <Route path="/contact" element={<Contact/>} />
-    </Routes>
+    </Routes> */}
     </>
   );
 }
